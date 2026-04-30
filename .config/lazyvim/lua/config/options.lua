db31a25f26ebe7044fc1654b37537cf1794a5947
Anthony Kim <236494448+anthonykim-hub@users.noreply.github.com>
2026-04-30 09:35:09 -0500

    differentiate WSL vs non-WSL better for clipboard

--- .config/lazyvim/lua/config/options.lua
+++ .config/lazyvim/lua/config/options.lua
@@ -1,12 +1,17 @@
-vim.g.clipboard = 'win32yank'
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.python3_host_prog = '~/Projects/py3nvim/.venv/bin/python3'
 vim.g.node_host_prog = '~/.local/bin/neovim-node-host'
 vim.g.lazyvim_python_lsp = 'ty'
 vim.g.lazyvim_python_ruff = 'ruff'
+if vim.env.WSL_DISTRO_NAME then
+  vim.g.clipboard = 'win32yank'
+end
 if vim.env.SSH_TTY and not vim.env.TMUX then
   vim.g.clipboard = 'osc52'
+end
+if vim.env.TMUX then
+  vim.g.clipboard = 'tmux'
 end
 vim.opt.background = 'dark'
 vim.opt.clipboard = 'unnamedplus,unnamed'
